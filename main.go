583db79dc31892fdddd0c42b77843b3c6aa60ef0
Rubén Caro <ruben.caro.estevez@gmail.com>
2018-03-03 12:19:12 +0100

    All set

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	// "golang.org/x/net/html"
+	"github.com/PuerkitoBio/goquery"
+)
 
 func main() {
-	fmt.Println("hey")
+	// resp, err := http.Get("http://blog.admanmedia.com")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// body := resp.Body
+	// defer body.Close()
+
+	// text, err := ioutil.ReadAll(body)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// doc, err := html.Parse(body)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println(doc.FirstChild.Data)
+
+	doc, err := goquery.NewDocument("http://blog.admanmedia.com")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	doc.Find(".entry-title a").Each(func(i int, s *goquery.Selection) {
+		href, _ := s.Attr("href")
+		fmt.Printf("%s\n(%s)\n\n", s.Text(), href)
+	})
+
 }
